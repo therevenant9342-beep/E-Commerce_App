e2a62e179e9845318f030f4a0f57df7b1e14db0e
Ali Khaleel <therevenant9342@gmail.com>
2026-07-29 14:46:07 +0300

    Implement responsive global layout, routing, and UI shell

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Products } from './pages/products/products';
+import { Register } from './pages/register/register';
+import { Login } from './pages/login/login';
+import { Cart } from './pages/cart/cart';
+import { Checkout } from './pages/checkout/checkout';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home}, 
+  { path: 'products', component: Products },
+  { path: 'register', component: Register },
+  { path: 'login', component: Login },
+  { path: 'cart', component: Cart },
+  { path: 'checkout', component: Checkout },
+  { path: 'about', component: About },
+  { path: 'contact', component: Contact },
+  { path: '**', redirectTo: '' } 
+];
